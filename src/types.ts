3d33f8b2c1de04352c50237408b95c39bb6310cd
RyanPeyrot <ryan.peyrot@gmail.com>
2024-01-31 11:36:23 +0100

    Nouveau types.

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,37 @@
 export interface Badge {
-    id: String;
+    id: Number;
     fill: String;
     nom: String;
     categorie: String;
+		description: String;
+		owner_id:Number;
+}
 
+export interface Login {
+	id: Number;
+	classe: String;
+	fill: String;
+	filliere: String;
+	mail: String;
+	mdp: String;
+	nom: String;
+	prenom: String;
+}
+
+export interface Owner {
+	id: Number;
+	nom: String;
+}
+
+export interface OwnerRelation {
+	badge_id: Number;
+	owner_id:Number;
+}
+
+export interface Request {
+	id: Number;
+	com: String;
+	idUser: Number;
+	obj: String;
+	preuve:Boolean;
 }
